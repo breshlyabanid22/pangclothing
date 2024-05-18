5a4f35cbc3a97cd7df3d9a2424554fe07552ed81
Breshly U. Abanid <breshlyabanid@gmail.com>
2024-05-19 00:46:50 +0800

    Updated the xml (added recycler view)

--- app/src/main/java/com/example/pangclothing/Cart.java
+++ app/src/main/java/com/example/pangclothing/Cart.java
@@ -3,13 +3,33 @@ package com.example.pangclothing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Cart extends AppCompatActivity {
+
+    private ImageView productImage;
+    private TextView productName;
+    private TextView description;
+    private TextView price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        productImage = findViewById(R.id.productImage);
+        productName = findViewById(R.id.productName);
+        description = findViewById(R.id.description);
+        price = findViewById(R.id.price);
+
     }
 }
 
